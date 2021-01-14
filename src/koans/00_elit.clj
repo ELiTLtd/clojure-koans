d2931dc895af9c1fdd679589f1353a2dbd86fa99
THE MATT <matt@mthompson.org>
2021-01-14 13:50:04 +0000

    Changed order

--- src/koans/00_elit.clj
+++ src/koans/00_elit.clj
@@ -4,6 +4,9 @@
 (defn square [n] (* n n))
 
 (meditations
+ "Calling a function is like giving it a hug with parentheses"
+ (= __ (square 9))
+
  "You can combine and nest expressions for evaluation"
  (= (+ 3 4) 7 (+ 2 __))
 
@@ -14,9 +17,6 @@
  (= __ (if (false? (= 4 5))
          :a
          :b))
-
- "Calling a function is like giving it a hug with parentheses"
- (= __ (square 9))
 
  "The map function relates a sequence to another"
  (= [__ __ __] (map (fn [x] (* 4 x)) [1 2 3]))
